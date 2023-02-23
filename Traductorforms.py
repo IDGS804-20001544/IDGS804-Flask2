3f6ebc5567eb1f871c5db2b2ab905046c789fb55
Jorge Hernández <20001544@alumnos.utleon.edu.mx>
2023-02-23 14:29:48 -0600

    23/02/2023 Traductor

--- Traductorforms.py
+++ Traductorforms.py
@@ -5,9 +5,7 @@ from wtforms import StringField,SubmitField,FieldList,FormField, SelectField, Ra
 from wtforms.fields import EmailField, TextAreaField, PasswordField,RadioField
 from wtforms import validators
 
-#def mi_validacion(form,field):
- #   if len(field.data)==0:
-  #      raise validators.ValidationError('El campo no tiene datos')
+
     
 class Traduform(Form):
     español= StringField('Español',[validators.DataRequired(message="El campo es requerido"), 
@@ -16,8 +14,7 @@ class Traduform(Form):
     ingles=StringField('Ingles',[validators.DataRequired(message="El campo es requerido"), 
                                  validators.length(min=1)])
 
-class Traducidoform(Form):
-    traducir=StringField('Traducir')
+
 
     
     
